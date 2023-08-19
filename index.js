7aaead3bec482dbbaba1ac531cc5ebd3a29be2f9
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-08-19 14:56:34 -0400

    week

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('hello')
+
+
+
+//get dates for the week
+
+let curr = new Date 
+let week = []
+
+for (let i = 0; i <= 6; i++) {
+  let first = curr.getDate() - curr.getDay() + i 
+  let day = new Date(curr.setDate(first)).toISOString().slice(0, 10)
+  week.push(day)
+}
+
+console.log(week)
+/*
+function dates(current) {
+    var week= new Array(); 
+    // Starting Monday not Sunday
+    current.setDate((current.getDate() - current.getDay() +1));
+    for (var i = 0; i < 7; i++) {
+        week.push(
+            new Date(current)
+        ); 
+        current.setDate(current.getDate() +1);
+    }
+    return week; 
+}
+console.log(dates(new Date));*/
